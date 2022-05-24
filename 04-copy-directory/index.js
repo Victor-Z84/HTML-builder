172dfd550e40b46054e10ba08c5a893497783cfb
Victor-Z84 <praguewedphoto@gmail.com>
2022-05-24 12:22:53 +0200

    feat: completed task 04 - copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const pathToSourceFolder = path.join(__dirname, 'files');
+const pathToCopyFolder = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  await fs.rm(pathToCopyFolder, { recursive: true, force: true });
+  await fs.mkdir(pathToCopyFolder);
+
+  let elements = await fs.readdir(pathToSourceFolder, { withFileTypes: true });
+  for (let element of elements) {
+    await fs.copyFile(path.join(pathToSourceFolder, element.name), path.join(pathToCopyFolder, element.name));
+  }
+}
+
+copyDir();
